8a2c7958cd0276551d0cfc96dd8d795794a99ff2
Stephan <sgriesel@gmail.com>
2019-07-03 20:17:04 +0200

    finish

---
+++ script.js
@@ -0,0 +1,28 @@
+var image = document.getElementsByClassName('ione');
+      new simpleParallax(image, {
+            scale: 1.8
+      });
+      var image = document.getElementsByClassName('itwo');
+      new simpleParallax(image, {
+            scale: 1.6,
+            orientation: 'left'
+      });
+      var image = document.getElementsByClassName('ithree');
+      new simpleParallax(image, {
+            scale: 1.6,
+            orientation: 'right'
+      });
+      var image = document.getElementsByClassName('ifour');
+      new simpleParallax(image, {
+            scale: 1.6,
+      });
+      var image = document.getElementsByClassName('ifive');
+      new simpleParallax(image, {
+            scale: 1.6,
+            orientation: 'right'
+      });
+      var image = document.getElementsByClassName('isix');
+      new simpleParallax(image, {
+            scale: 1.6,
+            orientation: 'left'
+      });
